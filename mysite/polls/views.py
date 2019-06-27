a6c10942e63315c26ddf20144d2b406f89de2557
kaique.xavier <kaique.xavier@lemeconsultoria.com.br>
2019-06-27 19:32:45 -0300

    Continuação do Django Tutorial parte4.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
+
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request,'polls/detail.html', context)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question':  question,
+        'error_message': "You didn't select a choice.",
+    }
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1 
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
