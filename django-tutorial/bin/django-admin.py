2eca38865e106cf1ba539c81d21a197fce29292d
X-X-Xavito <kaiquexatalla@gmail.com>
2019-07-06 00:15:24 -0300

    Fim do tutorial. Sem as partes de teste e do Django-Admin

--- django-tutorial/bin/django-admin.py
+++ django-tutorial/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/xavito/Desktop/DjangoProject/django-tutorial/bin/python3
+#!/home/xavito/Desktop/Django-Project/DjangoProject/django-tutorial/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
